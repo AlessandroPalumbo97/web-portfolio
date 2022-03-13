a6481ae4b1cec64594442abe5dfa4d3ae0beb220
Alessandro Palumbo <mr.alessandropalumbo@gmail.com>
2022-03-13 15:01:55 +0100

    Home align and text

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -11,41 +11,15 @@ const Home = () => {
 
   return (
     <Row className={styles.homeMainRow}>
-      <Col className={styles.imgCol} lg={12}>
-        <img alt="my eye :)" src={MyEye} />
+      <Col className={styles.imgCol} xs={24} md={8}>
+        <img className={styles.eyeImg} alt="my eye :)" src={MyEye} />
       </Col>
-      <Col lg={12}>
-        <Hero title={home.title} />
-        <div style={{ padding: 20 }}>
-          <p>
-            My Name is Alessandro Palumbo and I was born in Milan in 1997.
-            Friends used to call me Alex, or just “P”. I started play Doom on
-            Windows97 when I was four and fell In love with it. Few years later,
-            I started watching my daddy programming Java. So my curiosity for
-            videogames and technology grew up year by year.
-          </p>
-          <p>
-            In July 2020 I got my degree from European Institute of Design of
-            Milan in Media Design with a mark of 109/110. There, I started
-            showing interest in design, photography, videoediting, 3D print and
-            a bit of robotics.
-          </p>
-          <p>
-            From November 2020 I am employed as a software developer at FME
-            Education
-          </p>
-          <p>
-            I made a lot of different experiences through these years of life,
-            but I still feel like my journey is at the beginning :).
-          </p>
-          <p>
-            For any other information, to ask for a collaboration, or just to
-            have a conversation, don’t hesitate to write me at{' '}
-            <a href="mailto:mr.alessandropalumbo@gmail.com">
-              mr.alessandropalumbo@gmail.com.
-            </a>
-          </p>
-        </div>
+      <Col
+        xs={24}
+        md={16}
+        // style={{padding: '200 200 200 0'}}
+      >
+        <Hero title={home.title} subTitle={home.subTitle} text={home.text} />
       </Col>
     </Row>
   );
--- src/settings/hero/HeroItems.js
+++ src/settings/hero/HeroItems.js
@@ -1,11 +1,12 @@
+import React from 'react';
+import { Link } from 'umi';
 import me from '@/assets/images/me.jpg';
 
 export const HERO_ITEMS = {
   home: {
     title: 'Alessandro Palumbo',
-    subTitle: 'Welcome to my crazy place 🤪',
-    text:
-      'Check out my projects below, contact me, or just have fun playing Salvinification',
+    subTitle: 'I like to call myself a "nerd", but i have many habits 🤪',
+    text: <Link to="/about">read more...</Link>,
   },
   aurora: {
     title: 'Aurora',
